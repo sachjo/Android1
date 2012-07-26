e3bbac2c1a16e747ba777e74a74d38448a3a4deb
sachjo <sachjo@gmail.com>
2012-07-26 22:20:39 +0900

    Set Activity to textview

--- src/sachjo/android1/MainActivity.java
+++ src/sachjo/android1/MainActivity.java
@@ -3,6 +3,7 @@ package sachjo.android1;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +11,9 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        TextView textView1 = (TextView) findViewById(R.id.textView1);
+        textView1.setText("こんにちはメインアクティビティ");
     }
 
     @Override
